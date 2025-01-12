2a6b214c9674f3e4d0931fbca1cbbc36dad12eb7
CJ Davis <carliebug812@gmail.com>
2025-01-12 17:11:02 -0600

    20

---
+++ Resources/java/script.js
@@ -0,0 +1,8 @@
+document.getElementById("num1").onclick = function write1()
+    {
+        document.getElementById("screen").innerHTML ="1";
+    }
+document.getElementById("clear").onclick = function write1()
+    {
+        document.getElementById("screen").innerHTML ="";
+    }
--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("num1").onclick = function write1()
-    {
-        doncument.getElementById("screen").innerHTML ="1";
-    }
